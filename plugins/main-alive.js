2b94e7a7c36065f7e1b16c768d1384e1b6a73ac7
UchihaObito786 <148492063+UchihaObito786@users.noreply.github.com>
2024-05-28 19:46:26 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender)
-  var vn = 'https://cdn.jsdelivr.net/gh/Guru322/GURU-BOT@main/Assets/mp3/Alive.mp3'
+  var vn = 'https://i.imgur.com/1gNOp86.mp4'
   let url = 'https://github.com/Obito786/OBI-BOT'
   let murl = 'https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i'
   let img = 'https://cdn.wallpapersafari.com/71/19/7ZfcpT.png'
