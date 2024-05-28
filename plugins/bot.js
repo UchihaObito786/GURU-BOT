d0ae11fc594aae56ca2b288f93573b4856c6b39e
UchihaObito786 <148492063+UchihaObito786@users.noreply.github.com>
2024-05-28 19:40:30 +0530

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
 
-handler.customPrefix = /^(bot|guru)$/i
+handler.customPrefix = /^(bot|Obi)$/i
 handler.command = new RegExp()
 
 export default handler
